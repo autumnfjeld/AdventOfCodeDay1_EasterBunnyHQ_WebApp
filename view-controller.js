99b463bc29ed61e85a582431ef46a9bab32462ea
Autumn Fjeld <autumn@talentbin.com>
2017-10-22 19:33:39 -0700

    Display basic canvas element with coordinate axis

--- view-controller.js
+++ view-controller.js
@@ -24,7 +24,41 @@
         this.$input.innerHTML= this.sequence;
         this.$solution.innerHTML = this.minBlocks;
 
+        this.setUpCanvas();
+
     };
+
+    ViewController.prototype.setUpCanvas = function(){
+
+        var gridLineColor = 'hsl(0,0%,80%)',
+            axisColor = 'hsl(0,0%,60%)';
+
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d"),
+            transX = canvas.width * 0.5,
+            transY = canvas.height * 0.5;
+
+
+        // Move x and y axis to center of canvas
+        context.translate(transX, transY);
+        // Mirror y axis to create Cartesian coordinate system
+        context.scale(1,-1);
+        context.lineWidth = 1;
+        context.strokeStyle = axisColor;
+
+        // tell canvas you are beginning a new path
+        context.beginPath();
+        var xMin = -canvas.width/2, xMax = canvas.width/2;
+        var yMin = -canvas.height/2, yMax = canvas.height/2;
+
+        // Draw coordinates axis
+        context.moveTo(xMin, 0);
+        context.lineTo(xMax, 0);
+        context.moveTo(0, yMin);
+        context.lineTo(0, yMax);
+        context.stroke();
+    };
+
 
     // Export to window
     window.app = window.app || {};
