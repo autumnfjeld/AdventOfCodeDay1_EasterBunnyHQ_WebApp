f991891ef14015683182382f9e564889a59cce92
Autumn Fjeld <autumn@talentbin.com>
2017-10-22 19:53:46 -0700

    Refactor variable names

--- view-controller.js
+++ view-controller.js
@@ -9,9 +9,10 @@
      * @param {number} minBlocks
      * @constructor
      */
-    function ViewController(sequence, minBlocks) {
+    function ViewController(sequence, position, minBlocks) {
         this.sequence = sequence;
         this.minBlocks = minBlocks;
+        this.pathCoordinates = position.pathCoordinates;
 
         this.$input = document.querySelector('.input');
         this.$solution = document.querySelector('.solution');
@@ -23,6 +24,7 @@
     ViewController.prototype.render = function(){
         this.$input.innerHTML= this.sequence;
         this.$solution.innerHTML = this.minBlocks;
+        this.$canvas = document.getElementById("canvas");
 
         this.setUpCanvas();
 
@@ -30,14 +32,12 @@
 
     ViewController.prototype.setUpCanvas = function(){
 
-        var gridLineColor = 'hsl(0,0%,80%)',
-            axisColor = 'hsl(0,0%,60%)';
+        var gridLineColor = 'hsl(0,0%,70%)',
+            axisColor = 'hsl(0,0%,50%)';
 
-        var canvas = document.getElementById("canvas");
-        var context = canvas.getContext("2d"),
-            transX = canvas.width * 0.5,
-            transY = canvas.height * 0.5;
-
+        var context = this.$canvas.getContext("2d"),
+            transX = this.$canvas.width * 0.5,
+            transY = this.$canvas.height * 0.5;
 
         // Move x and y axis to center of canvas
         context.translate(transX, transY);
@@ -46,16 +46,16 @@
         context.lineWidth = 1;
         context.strokeStyle = axisColor;
 
-        // tell canvas you are beginning a new path
+        // tell canvas you to begin a new path
         context.beginPath();
-        var xMin = -canvas.width/2, xMax = canvas.width/2;
-        var yMin = -canvas.height/2, yMax = canvas.height/2;
+        var minX = -this.$canvas.width/2, maxX = this.$canvas.width/2;
+        var minY = -this.$canvas.height/2, maxY = this.$canvas.height/2;
 
         // Draw coordinates axis
-        context.moveTo(xMin, 0);
-        context.lineTo(xMax, 0);
-        context.moveTo(0, yMin);
-        context.lineTo(0, yMax);
+        context.moveTo(minX, 0);
+        context.lineTo(maxX, 0);
+        context.moveTo(0, minY);
+        context.lineTo(0, maxY);
         context.stroke();
     };
 
