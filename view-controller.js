16ed5e1331f800b05485252e0bf49654658fc2c9
Autumn Fjeld <autumn@Orange.local>
2017-11-01 16:02:25 -0700

    WIP: refactor to size canvas according to given pathBoundaries

--- view-controller.js
+++ view-controller.js
@@ -12,10 +12,12 @@
     function ViewController(sequence, position, minBlocks) {
         this.sequence = sequence;
         this.minBlocks = minBlocks;
+        this.pathBoundaries = position.pathBoundaries;
         this.pathCoordinates = position.pathCoordinates;
         // Multiplier to make 10 pixels = 1 block
         // TODO organize canvas settings
-        this.scaleFactor = 20;
+        // Set the # of pixels in a city grid (TODO: will need to be computed for variable pathBoundaries)
+        this.scaleFactor = 5;
         this.pathColor = 'hsl(88, 48%, 41%)';
 
         this.$input = document.querySelector('.input');
@@ -23,7 +25,7 @@
     }
 
     /**
-     * Initiate render of dynamic DOM info
+     * Initiate render of DOM
      */
     ViewController.prototype.render = function(){
         this.$input.innerHTML= this.sequence;
@@ -31,27 +33,42 @@
         this.$canvas = document.getElementById("canvas");
         this.context = this.$canvas.getContext("2d"),
 
-        this.setUpCanvas();
-        this.drawPath();
+        this._setUpCanvas();
+        this._drawPath();
 
     };
 
-    // TODO change canvas size depending on path boundaries
-    ViewController.prototype.setUpCanvas = function(){
+    /**
+     * Sets up:
+     *   - canvas size based on pathBoundaries computed in FindEasterBunnyHQ
+     *   - x, y coordinates axes 
+     *   - grid lines that represent the city grid
+     * 
+     */
+    ViewController.prototype._setUpCanvas = function(){
 
-        this.$canvas.width = 200;
-        this.$canvas.height = 200;
+        var minX = this.pathBoundaries.minX * this.scaleFactor - 2 * this.scaleFactor,
+            maxX = this.pathBoundaries.maxX * this.scaleFactor + 2 * this.scaleFactor,
+            minY = this.pathBoundaries.minY * this.scaleFactor - 2 * this.scaleFactor,
+            maxY = this.pathBoundaries.maxY * this.scaleFactor + 2 * this.scaleFactor;
+            console.log('Grid boundaries', minX, maxX, minY, maxY);
 
+        this.$canvas.width =  (Math.abs(minX) + maxX);
+        this.$canvas.height = (Math.abs(minY) + maxY);
         var gridLineColor = 'hsl(0,0%,70%)',
-            axisColor = 'hsl(0,0%,50%)';
+            axisColor = 'hsl(0,100%,100%)';
 
         //TODO use this.context
+        // Canvas puts [0,0] point at top left, with x+ going right and y positive
+        // going down.  Use canvas translate and scale methods to convert the x, y axis
+        // into the familiar cartision grid coordinate directions
         var context = this.context,
-            transX = this.$canvas.width * 0.5,
-            transY = this.$canvas.height * 0.5;
+            translateXAxix = Math.abs(minX),
+            translateYAxis = maxY;
 
         // Move x and y axis to center of canvas
-        context.translate(transX, transY);
+        context.translate(translateXAxix, translateYAxis);
+
         // Mirror y axis to create Cartesian coordinate system
         context.scale(1,-1);
         context.lineWidth = 1;
@@ -59,22 +76,27 @@
 
         // tell canvas you to begin a new path
         context.beginPath();
-        var minX = -this.$canvas.width/2, maxX = this.$canvas.width/2;
-        var minY = -this.$canvas.height/2, maxY = this.$canvas.height/2;
+        // var minX = -this.$canvas.width/2, maxX = this.$canvas.width/2;
+        // var minY = -this.$canvas.height/2, maxY = this.$canvas.height/2;
 
-        // Draw coordinates axis
+
+        // Draw x coordinate axis
         context.moveTo(minX, 0);
         context.lineTo(maxX, 0);
+        // Draw y axis
         context.moveTo(0, minY);
         context.lineTo(0, maxY);
         context.stroke();
 
         // Draw grid lines
-        var gridLineInterval = 20;
+        var gridLineInterval = this.scaleFactor;
         var x = minX, y = minY;
-        while (y < maxY) {
-            var	x = x + gridLineInterval;
+        // TODO account for non-equal x,y iterations 
+        while (y < maxY || x < maxX) {
+            // console.log('before x', x, ' y', y);
+            var x = x + gridLineInterval;
             var y = y + gridLineInterval;
+            // console.log('after', x, ' y', y);
             context.lineWidth = .5;
             context.strokeStyle = gridLineColor;
             context.beginPath();
@@ -92,14 +114,18 @@
     /**
      * Draw the path resulting from the input sequence instructions
      */
-    ViewController.prototype.drawPath = function(){
+    ViewController.prototype._drawPath = function(){
         this.context.lineWidth = 2;
         this.context.strokeStyle = this.pathColor;
         this.context.beginPath();
         this.context.moveTo(0,0);  // put in variable
         // TODO animate path drawing and coordinate display of the sequence instruction
         this.pathCoordinates.forEach(function(coord){
-            this.context.lineTo(coord.x * this.scaleFactor, -coord.y * this.scaleFactor);
+            var x = coord.x * this.scaleFactor,
+                y = coord.y * this.scaleFactor;
+            console.log(' x', x, ' y', y);
+
+            this.context.lineTo(x, y);
             this.context.stroke();
         }.bind(this));
     };
